6224c58da1a572330795147408bcf4add31aa92d
Elias Bosire Ayunga <bosire.elias@gmail.com>
2024-12-05 06:03:15 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// 1. customerName
+var customerName = 'bob';
+
+// 2.upper case
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+  }
+  
+//3. setBestCustomer
+function setBestCustomer() {
+    bestCustomer = 'not bob'; // Declare bestCustomer in global scope
+  }
+  
+  //4. bestCustomer
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+
+  // 5. leastFavoriteCustomer
+  const leastFavoriteCustomer = 'AUG';
+
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Bob';
+  }
